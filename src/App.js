dc0d745bb6fda4deaaf2251030974db3b7939a74
Krishnakumar <krishnakumars0101@gmail.com>
2024-10-24 15:08:24 +0530

    main content finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Mainpage from "./components/Mainpage";
 
 export default function App() {
     return (
         <>
         <Navbar/>
+        <Mainpage/>
         </>
     )
 }
---
+++ src/components/Mainpage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Mainpage() {
+    return (
+        <section className="hero">
+            <div >
+                <img  className="hero-img" src ="./Group 77.png"/>
+            </div>
+            <div >
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                </p>
+            </div>
+        </section>
+    )
+}
